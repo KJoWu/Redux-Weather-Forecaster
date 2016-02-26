3c2b9ce2ae3cb44cdb8683ec8f45ad67d3c0c325
Silver Rook <kwu1@ualberta.ca>
2016-02-25 19:58:50 -0700

    added weather list

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,11 @@
+import { FETCH_WEATHER} from "../actions/index";  //imported the FETCH_WEATHER
 
-export default function(state = null, action) {
-console.log("Action recieved", action);
-  return state;
+export default function(state = [], action) {
+	switch(action.type){
+		case FETCH_WEATHER:
+			return [action.payload.data, ...state ]; //this is the only data we want. Add on to existing state
+	} //do not use state.push. Use state.concat or the above
+
+	console.log("Action recieved", action);
+	return state;
 }

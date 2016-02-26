0f7720956388205b8cdbcc979a168938845ccb95
Silver Rook <kwu1@ualberta.ca>
2016-02-25 20:42:00 -0700

    Added units to forecast

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,14 +1,27 @@
 //does not need any state. Props just come in. So we use function over className
 import React from 'react';
-import {Sparklines, SparklinesLine} from 'react-sparklines';
+import _ from 'lodash'; //npm install --save lodash
 
+import {Sparklines, SparklinesLine, SparklinesReferenceLine} from 'react-sparklines';
+
+function average(data){ //calculates the average
+	return _.round(_.sum(data)/data.length);
+}
 
 export default (props) =>{
 	return (
 		<div>
 			<Sparklines height={120} width={180} data={props.data}>
 				<SparklinesLine color={props.color} />
+				<SparklinesReferenceLine type="avg" />
 			</Sparklines>
+
+				<div>{average(props.data)} {props.units}</div> 
+
 		</div>
 	);
 }
+
+
+
+

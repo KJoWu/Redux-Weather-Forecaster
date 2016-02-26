d50ff6e4c9f11fcc079453fbcc276ca823c18929
Silver Rook <kwu1@ualberta.ca>
2016-02-25 17:41:21 -0700

    added binding to searchbar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,15 +1,34 @@
 import React, {Component} from "react";
 
 export default class SearchBar extends Component{
+	//initializing state
+	constructor(props){
+		super(props);
+		this.state = {term:''};
+		this.onInputChange = this.onInputChange.bind(this); 
+		//this instance of searchbar has a function called onInputChange. Bind that function to this(Search Bar)
+	}
+
+	onInputChange(event){ //onHover,onChange ect. uses event object
+		console.log(event.target.value);
+		this.setState({term: event.target.value});
+	}
+
 	render(){
 		return (
-			<form className="input-group">
-				<input />
+			<form className="input-group"> 
+			//when you press enter on a form element, 
+			//the browser thinks you're submitting contents on a form
+				<input 
+					placeholder="Get a five day forecast in your favorite cities"
+					className="form-control"
+					value={this.state.term} //turns it into controlled component
+					onChange={this.onInputChange} />
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-secondary"> Submit </button>
 				</span>
 			</form>
-		)
+		);
 	}
 
 }
